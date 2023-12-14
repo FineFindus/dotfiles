d644155152daad41b1d91d2bfe95d900304dae06
FineFindus <FineFindus@proton.me>
2023-12-14 16:32:55 +0100

    feat(nvim): directly compare hostname with value

--- .config/nvim/lua/device.lua
+++ .config/nvim/lua/device.lua
@@ -3,7 +3,7 @@ local hostname = vim.fn.hostname()
 
 local M = {}
 
-M.isLaptop = string.find(hostname, '?')
-M.isDesktop = string.find(hostname, 'archlinux')
+M.isLaptop = hostname == '?'
+M.isDesktop = hostname == 'archlinux'
 
 return M
